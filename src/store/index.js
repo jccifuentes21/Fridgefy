4ac7924e0ad4c250454ad14c21bc5850b66f2e3e
Juan Camilo <jccifuentes21@gmail.com>
2022-08-29 09:49:38 -0700

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+//ALL REDUX LOGIC GOES HERE
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  // reducer: { all the slice reducers would go here !}
+})
+
+export default store;
