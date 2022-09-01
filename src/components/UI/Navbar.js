c452d376e2309a9507942bcd045eb8376a6a7c9a
Vini Guimaraes <niciusfg@gmail.com>
2022-08-31 22:27:00 -0700

    new images + html css main page

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -3,13 +3,18 @@ import classes from "./Navbar.module.css";
 const Navbar = () => {
   return (
     <div className={classes["nav-bar"]}>
-      <h1>Frigefy</h1>
+      <img src="./images/logo.png"/>
+
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Recipes</a></li>
+          <li><a href="#">My shopping list</a></li>
+        </ul>
+ 
       <div className={classes['button-group']}>
-        <button className={classes['nav-buttons']}>Recipes</button>
-        <button className={classes['nav-buttons']}>My Shopping List</button>
         <p>Hello, user!</p>
-        <button className={classes['log-buttons']}>Login</button>
-        <button className={classes['log-buttons']}>Logout</button>
+        <button className={classes['log-buttons']}> <img src="./images/btn-login.png"/> Login</button>
+        {/* <button className={classes['log-buttons']}> <img src="./images/btn-logout.png"/> Logout</button> */}
       </div>
     </div>
   );
