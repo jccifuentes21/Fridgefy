fab9883ecce47a028c50c93f4d00bd9a0924525c
Juan Camilo <jccifuentes21@gmail.com>
2022-09-05 17:47:33 -0700

    removed the add button for the recipes from the not logged in pages

--- src/components/Recipes/RecipeItem.js
+++ src/components/Recipes/RecipeItem.js
@@ -1,22 +1,25 @@
-import classes from './RecipeItem.module.css'
+import { useContext } from "react";
+import AuthContext from "../../store/auth-context";
+import classes from "./RecipeItem.module.css";
 
 const RecipeItem = (props) => {
-  const onAdd = () =>{
+  const { isLoggedIn } = useContext(AuthContext);
+  const onAdd = () => {
     props.onAdd({
       id: props.id,
       title: props.title,
-      image: props.image
-    })
-  }
+      image: props.image,
+    });
+  };
   return (
     <div className={classes["box-recipe"]}>
       <img className={classes.image} src={props.image} />
       <div className={classes["more-infos"]}>
         {" "}
         <span>{props.title} </span>{" "}
-        <button onClick={onAdd}>
+        {isLoggedIn && <button onClick={onAdd}>
           <img src="./images/btn-add.png" />
-        </button>
+        </button>}
       </div>
     </div>
   );
